a88c63b47698960ca5449efee4320214b94289f8
Samuel Lee <mountain7cat@gmail.com>
2016-01-06 15:58:24 -0800

    update card view constructor

--- app/src/main/java/mountaincat/dominion/ui/CardView.java
+++ app/src/main/java/mountaincat/dominion/ui/CardView.java
@@ -1,10 +1,20 @@
 package mountaincat.dominion.ui;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
 public class CardView extends LinearLayout {
+
 	public CardView( Context context ) {
 		super( context );
+	}
+
+	public CardView( Context context, AttributeSet attrs ) {
+		super( context, attrs );
+	}
+
+	public CardView( Context context, AttributeSet attrs, int defStyleAttr ) {
+		super( context, attrs, defStyleAttr );
 	}
 }
