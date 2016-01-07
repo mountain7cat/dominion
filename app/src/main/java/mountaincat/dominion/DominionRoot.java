852859c581706c0896543df31faaec6d16ae2044
Samuel Lee <mountain7cat@gmail.com>
2016-01-06 18:42:17 -0800

    add animation stuff

--- app/src/main/java/mountaincat/dominion/DominionRoot.java
+++ app/src/main/java/mountaincat/dominion/DominionRoot.java
@@ -10,31 +10,68 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 
 public class DominionRoot extends FragmentActivity {
 
 	private Game mGame;
-	View mCardView;
+	private View mCardView;
+	private View mCardName;
+	private View mMiscellaneous;
+	private View mImg1;
+	private View mImg2;
+	private View mImg3;
+	private Button mRevealButton;
+	private Button mHideButton;
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.sample );
 		mCardView = findViewById( R.id.card );
+		mCardName = findViewById( R.id.cardName );
+		mMiscellaneous = findViewById( R.id.miscellaneous );
+		mImg1 = findViewById( R.id.img1 );
+		mImg2 = findViewById( R.id.img2 );
+		mImg3 = findViewById( R.id.img3 );
+		mRevealButton = (Button) findViewById( R.id.revealButton );
+		mHideButton = (Button) findViewById( R.id.hideButton );
+
 		mCardView.setOnClickListener( new View.OnClickListener() {
 			@Override
 			public void onClick( View v ) {
 				Intent intent = new Intent( DominionRoot.this, SecondActivity.class );
+				Pair<View, String> p1 = Pair.create( mCardName, "cardName" );
+				Pair<View, String> p2 = Pair.create( mMiscellaneous, "miscellaneous" );
+
 				ActivityOptionsCompat options =
-						ActivityOptionsCompat.makeSceneTransitionAnimation( DominionRoot.this, mCardView, "card" );
+						ActivityOptionsCompat.makeSceneTransitionAnimation( DominionRoot.this, p1, p2 );
 				startActivity( intent, options.toBundle() );
 			}
 		} );
 
+		mRevealButton.setOnClickListener( new View.OnClickListener() {
+			@Override
+			public void onClick( View v ) {
+				AnimationUtil.enterReveal( mImg1 );
+				AnimationUtil.enterReveal( mImg2 );
+				AnimationUtil.enterReveal( mImg3 );
+			}
+		} );
+
+		mHideButton.setOnClickListener( new View.OnClickListener() {
+			@Override
+			public void onClick( View v ) {
+				AnimationUtil.exitReveal( mImg1 );
+				AnimationUtil.exitReveal( mImg2 );
+				AnimationUtil.exitReveal( mImg3 );
+			}
+		} );
 
 		// TODO: 1/4/16 add a start game dialog box
 		List<Player> players = new ArrayList<>();
@@ -45,7 +82,6 @@ public class DominionRoot extends FragmentActivity {
 		mGame = new Game( players );
 		mGame.start();
 	}
-
 
 	@Override
 	public boolean onCreateOptionsMenu( Menu menu ) {
