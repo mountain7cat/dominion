bea30059c6641ecf3e8360dafb0fea73b6e8ae1d
Samuel Lee <mountain7cat@gmail.com>
2016-01-12 16:54:29 -0800

    add some more animation research

--- app/src/main/java/mountaincat/dominion/DominionRoot.java
+++ app/src/main/java/mountaincat/dominion/DominionRoot.java
@@ -1,5 +1,7 @@
 package mountaincat.dominion;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityOptionsCompat;
@@ -8,7 +10,7 @@ import android.support.v4.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 
 public class DominionRoot extends FragmentActivity {
@@ -20,8 +22,6 @@ public class DominionRoot extends FragmentActivity {
 	private View mImg1;
 	private View mImg2;
 	private View mImg3;
-	private Button mRevealButton;
-	private Button mHideButton;
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
@@ -33,37 +33,17 @@ public class DominionRoot extends FragmentActivity {
 		mImg1 = findViewById( R.id.img1 );
 		mImg2 = findViewById( R.id.img2 );
 		mImg3 = findViewById( R.id.img3 );
-		mRevealButton = (Button) findViewById( R.id.revealButton );
-		mHideButton = (Button) findViewById( R.id.hideButton );
 
-		mCardView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(DominionRoot.this, SecondActivity.class);
-				Pair<View, String> p1 = Pair.create(mCardName, "cardName");
-				Pair<View, String> p2 = Pair.create(mMiscellaneous, "miscellaneous");
-
-				ActivityOptionsCompat options =
-						ActivityOptionsCompat.makeSceneTransitionAnimation(DominionRoot.this, p1, p2);
-				startActivity(intent, options.toBundle());
-			}
-		});
-
-		mRevealButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				AnimationUtil.enterReveal(mImg1);
-				AnimationUtil.enterReveal(mImg2);
-				AnimationUtil.enterReveal(mImg3);
-			}
-		});
-
-		mHideButton.setOnClickListener( new View.OnClickListener() {
+		mCardView.setOnClickListener( new View.OnClickListener() {
 			@Override
 			public void onClick( View v ) {
-				AnimationUtil.exitReveal( mImg1 );
-				AnimationUtil.exitReveal( mImg2 );
-				AnimationUtil.exitReveal( mImg3 );
+				Intent intent = new Intent( DominionRoot.this, SecondActivity.class );
+				Pair<View, String> p1 = Pair.create( mCardName, "cardName" );
+				Pair<View, String> p2 = Pair.create( mMiscellaneous, "miscellaneous" );
+
+				ActivityOptionsCompat options =
+						ActivityOptionsCompat.makeSceneTransitionAnimation( DominionRoot.this, p1, p2 );
+				startActivity( intent, options.toBundle() );
 			}
 		} );
 
@@ -88,5 +68,41 @@ public class DominionRoot extends FragmentActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected( item );
+	}
+
+	public void animate( View view ) {
+		float scale = getResources().getDisplayMetrics().density;
+		ObjectAnimator anim1 = ObjectAnimator.ofFloat( mCardView,
+				"translationX", 20.0f * scale, 220.0f * scale );
+		anim1.setDuration( 1000 );
+		ObjectAnimator anim2 = ObjectAnimator.ofFloat( mCardView,
+				"translationY", 20.0f * scale, 220.0f * scale );
+		anim2.setDuration( 1000 );
+		ObjectAnimator anim3 = ObjectAnimator.ofFloat( mCardView,
+				"translationX", 220.0f * scale, 20.0f * scale );
+		anim3.setDuration( 1000 );
+		ObjectAnimator anim4 = ObjectAnimator.ofFloat( mCardView,
+				"translationY", 220.0f * scale, 20.0f * scale );
+		anim4.setDuration( 1000 );
+		AnimatorSet animSet = new AnimatorSet();
+		animSet.play( anim1 ).before( anim2 );
+		animSet.play( anim2 ).before( anim3 );
+		animSet.play( anim3 ).with( anim4 );
+		animSet.play( anim1 ).after( 500 );
+		animSet.setInterpolator( new AccelerateDecelerateInterpolator() );
+		animSet.start();
+
+	}
+
+	public void reveal( View view ) {
+		AnimationUtil.enterReveal( mImg1 );
+		AnimationUtil.enterReveal( mImg2 );
+		AnimationUtil.enterReveal( mImg3 );
+	}
+
+	public void hide( View view ) {
+		AnimationUtil.exitReveal( mImg1 );
+		AnimationUtil.exitReveal( mImg2 );
+		AnimationUtil.exitReveal( mImg3 );
 	}
 }
